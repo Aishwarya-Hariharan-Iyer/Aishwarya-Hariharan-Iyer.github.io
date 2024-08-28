a24a5a937d00d0d62e0f1679ea0b8236d19f37e1
Aishwarya Hariharan Iyer <95858016+Aishwarya-Hariharan-Iyer@users.noreply.github.com>
2024-08-28 22:36:24 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/aishwarya-hariharan-iyer.github.io",
+    output: "export",
+    reactStrictMode: true,
     images: {
       domains: [
         "api.microlink.io", // Microlink Image Preview
